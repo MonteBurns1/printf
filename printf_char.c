6675bd0164777e15ba259953e4a526c8f5da34ff
MonteBurns1 <144336632+MonteBurns1@users.noreply.github.com>
2023-11-15 16:10:58 +0200

    lfk

--- printf_char.c
+++ printf_char.c
@@ -1,17 +1,15 @@
-/**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	char c = va_arg(types, int);
+#include "main.h"
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+/**
+ * printf_char - prints a char.
+ * @val: arguments.
+ * Return: 1.
+ */
+int printf_char(va_list val)
+{
+	char s;
+
+	s = va_arg(val, int);
+	_putchar(s);
+	return (1);
 }
