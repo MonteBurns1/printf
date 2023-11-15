7a3fc604561efee4d17c18e78134fef7aa132f9a
MonteBurns1 <zx998@outlook.sa>
2023-11-15 16:02:19 +0200

    aa

--- printf_char.c
+++ printf_char.c
@@ -1,16 +1,17 @@
-#include "main.h"
-
 /**
- * printf_char - prints a binary number
- * @args: the arguements to use
- * @printed: the printed characters
- * Return: printed charcaters
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-
-int printf_char(va_list args, int printed)
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	int character = va_arg(args, int);
+	char c = va_arg(types, int);
 
-	_putchar(character);
-	return (printed + 1);
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
